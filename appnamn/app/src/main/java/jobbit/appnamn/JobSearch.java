b8317494e736c1260890fa876b9b784558a5e9d9
MattiasJo <mattiasj81@gmail.com>
2016-05-13 16:03:42 +0200

    Skapade två vyer

--- appnamn/app/src/main/java/jobbit/appnamn/JobSearch.java
+++ appnamn/app/src/main/java/jobbit/appnamn/JobSearch.java
@@ -7,12 +7,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class StartScreen extends AppCompatActivity {
+public class JobSearch extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start_screen2);
+        setContentView(R.layout.activity_job_search);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
